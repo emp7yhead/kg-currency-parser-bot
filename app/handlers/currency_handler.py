7f858d98d7f155f99a704aa5fc5fc0367d15c9c0
Artyom Kropp <artyomkropp@gmail.com>
2022-11-04 02:00:02 +0900

    update commands

--- app/handlers/currency_handler.py
+++ app/handlers/currency_handler.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters.text import Text
+from aiogram.filters import Command, text
 from aiogram.types import Message
 from tables import TableHandler, parse_url_for_table, update_indexes
 
@@ -8,7 +8,10 @@ URL = 'https://valuta.kg/'
 router = Router()
 
 
-@router.message(Text(text='Get average course', ignore_case=True))
+@router.message(
+    text.Text(text='Get average course', ignore_case=True),
+    Command(commands=['average'])
+)
 async def send_average_table(message: Message):
     parsed_tables = parse_url_for_table(URL)
     table = TableHandler(parsed_tables).get_average()
@@ -16,7 +19,10 @@ async def send_average_table(message: Message):
     await message.reply(f'{table_with_indexes}')
 
 
-@router.message(Text(text='Get best course', ignore_case=True))
+@router.message(
+    text.Text(text='Get best course', ignore_case=True),
+    Command(commands=['best'])
+)
 async def send_best_table(message: Message):
     parsed_tables = parse_url_for_table(URL)
     table = TableHandler(parsed_tables).get_best_course()
@@ -24,8 +30,11 @@ async def send_best_table(message: Message):
     await message.reply(f'{table_with_indexes}')
 
 
-@router.message(Text(text='Get recomended course', ignore_case=True))
-async def send_recomended_table(message: Message):
+@router.message(
+    text.Text(text='Get recommended course', ignore_case=True),
+    Command(commands=['recommended'])
+)
+async def send_recommended_table(message: Message):
     parsed_tables = parse_url_for_table(URL)
     table = TableHandler(parsed_tables).get_national_bank_course()
     table_with_indexes = update_indexes(table)
